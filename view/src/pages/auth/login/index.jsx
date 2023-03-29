1f86303a278a6e70c24021eb0c29c97a334043c8
nf-honarpishefard <nfhonarpishefard@gmail.com>
2023-03-29 19:23:43 +0430

    v1.8.0

--- view/src/pages/auth/login/index.jsx
+++ view/src/pages/auth/login/index.jsx
@@ -18,7 +18,7 @@ export const Login = () => {
         <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
             <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-              Sign in to your account
+              Log in to your account
             </h1>
             <form onSubmit={handleSubmit(onLogin)}>
               <TextField
@@ -55,7 +55,7 @@ export const Login = () => {
                   Remember me
                 </label>
               </div>
-              <Button loading={loading}>Submit</Button>
+              <Button classes="w-full justify-center" loading={loading}>Log in</Button>
             </form>
             <p className="text-sm font-light text-gray-500 dark:text-gray-400">
               Don’t have an account yet?{" "}
--- view/src/pages/auth/signup/index.jsx
+++ view/src/pages/auth/signup/index.jsx
@@ -8,7 +8,7 @@ export function Signup() {
 
   return (
     <section className="bg-gray-50 dark:bg-gray-900 py-10">
-      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen my-12">
         <div className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
           <img className="w-8 h-8 mr-2" src={logo} alt="logo" />
           W&M
@@ -16,7 +16,7 @@ export function Signup() {
         <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
             <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-              Create and account
+              Create an account
             </h1>
             <form onSubmit={handleSubmit(onRegister)}>
               <TextField
@@ -68,7 +68,9 @@ export function Signup() {
                   Remember me
                 </label>
               </div>
-              <Button loading={loading}>Submit</Button>
+              <Button classes="w-full justify-center" loading={loading}>
+                Create Account
+              </Button>
             </form>
             <p className="text-sm font-light text-gray-500 dark:text-gray-400">
               Already have an account?{" "}
@@ -76,7 +78,7 @@ export function Signup() {
                 to="/login"
                 className="font-medium text-primary-600 hover:underline dark:text-primary-500"
               >
-                Login here
+                Log in here
               </Link>
             </p>
           </div>
