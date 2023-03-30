bc57289a9c84583c3133b1c7990d2f3742a96ddf
nf-honarpishefard <nfhonarpishefard@gmail.com>
2023-03-30 21:45:37 +0430

    v1.9.2

--- view/src/components/Modal/index.jsx
+++ view/src/components/Modal/index.jsx
@@ -2,7 +2,12 @@ import { Button } from "components";
 import { Modal } from "flowbite-react";
 import React, { useState } from "react";
 
-export const ModalComponent = ({ onClick }) => {
+export const ModalComponent = ({
+  cancel,
+  submit,
+  onClick,
+  children,
+}) => {
   const [visible, setVisible] = useState(false);
 
   return (
@@ -28,32 +33,19 @@ export const ModalComponent = ({ onClick }) => {
         <Modal.Header />
         <Modal.Body>
           <div className="text-center flex flex-col items-center">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="36"
-              height="36"
-              fill="#979797"
-              className="bi bi-exclamation-circle my-6"
-              viewBox="0 0 16 16"
-            >
-              <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
-              <path d="M7.002 11a1 1 0 1 1 2 0 1 1 0 0 1-2 0zM7.1 4.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 4.995z" />
-            </svg>
-            <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
-              Are you sure you want to delete this product?
-            </h3>
+            {children}
             <div className="flex justify-center gap-4">
               <button
                 onClick={onClick}
                 className="bg-red-700 text-white px-3 py-1 rounded-md"
               >
-                Yes, I'm sure
+                {submit}
               </button>
               <button
                 className="text-gray-400 px-5 py-1 border rounded-md"
                 onClick={() => setVisible(false)}
               >
-                No, cancel
+                {cancel}
               </button>
             </div>
           </div>
--- view/src/pages/card/index.jsx
+++ view/src/pages/card/index.jsx
@@ -60,7 +60,26 @@ export const CardScreen = () => {
                       i[1].quantity
                     ).then(() => setRemoved(!removed));
                   }}
-                />
+                  submit="Yes, I'm sure"
+                  cancel="No, cancel"
+                >
+                  <div className="text-center flex flex-col items-center">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="36"
+                      height="36"
+                      fill="#979797"
+                      className="bi bi-exclamation-circle my-6"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                      <path d="M7.002 11a1 1 0 1 1 2 0 1 1 0 0 1-2 0zM7.1 4.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 4.995z" />
+                    </svg>
+                    <h3 className="mb-8 text-lg font-normal text-gray-500 dark:text-gray-400">
+                      Are you sure you want to delete this product?
+                    </h3>
+                  </div>
+                </ModalComponent>
               </BasketProducts>
             ))}
           </div>
