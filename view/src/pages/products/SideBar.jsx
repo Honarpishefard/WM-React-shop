854bac22e1657b170f4b0bafeec8cae5110b2dce
nf-honarpishefard <nfhonarpishefard@gmail.com>
2023-04-09 22:06:30 +0430

    v2.1.3

--- view/src/pages/products/SideBar.jsx
+++ view/src/pages/products/SideBar.jsx
@@ -1,25 +1,30 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { store } from "context";
 import "assets/style/index.css";
+import { useParams } from "react-router";
+import { fetchProductsService } from "api";
 
 export const SideBar = ({ sections }) => {
   const { products, setProducts } = useContext(store);
   const [filteredProducts, setFilteredProducts] = useState([]);
+  const { category } = useParams();
 
-  // console.log(filteredProducts);
+  const handleFilter = async (i) => {
+    const res = await fetchProductsService(category || "");
+      res.data.data.filter(async (item) => {
+        setFilteredProducts([]);
+        (await item.category[1]) === i ? filteredProducts.push(item) : null;
+        setProducts(filteredProducts);
+      });
+  };
 
   return (
     <div className="px-10 py-9 mb-10 flex flex-col gap-5 border-r">
       {sections.map((i) => (
-        <div className="bg-indigo-50 rounded-2xl py-3 px-12"
-          onClick={() => {
-            products.filter((item) => {
-              item.category[1] === i ? filteredProducts.push(item) : null;
-              setProducts(filteredProducts);
-            });
-          }}
-          key={i}
-        >
+        <div
+          className="bg-indigo-50 rounded-2xl py-3 px-12"
+          onClick={() => handleFilter(i)}
+          key={i}>
           {i}
         </div>
       ))}
