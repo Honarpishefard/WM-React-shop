bb686e8b9e6a612ae23c43a96ecdcfb052d45c97
Honarpishefard <nfhonarpishefard@gmail.com>
2023-07-31 22:20:42 +0330

    update

--- src/api/constants.js
+++ src/api/constants.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
-const baseURL = 'http://localhost:3000/api';
-export const mediaURL = 'http://localhost:3000/statics/media/';
-export const uploadsURL = 'http://localhost:3000/statics/uploads/';
+
+const baseURL = 'http://localhost:5000/api';
+export const mediaURL = 'http://localhost:5000/statics/media/';
+export const uploadsURL = 'http://localhost:5000/statics/uploads/';
 
 export const mainInstance = axios.create({
     baseURL,
